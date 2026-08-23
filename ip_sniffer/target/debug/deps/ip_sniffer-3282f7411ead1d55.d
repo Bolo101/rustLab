97c8a0b18162f99d6731c2751cb618b993eb9c90
Bolo101 <iglesias.guillaumedu64@gmail.com>
2026-08-23 10:02:15 +0200

    Remove unnecessary target files and clean up build artifacts for ip_sniffer

--- ip_sniffer/target/debug/deps/ip_sniffer-3282f7411ead1d55.d
+++
@@ -1,5 +0,0 @@
-/home/bolo/Documents/GitHub/rustLab/ip_sniffer/target/debug/deps/ip_sniffer-3282f7411ead1d55.d: src/main.rs
-
-/home/bolo/Documents/GitHub/rustLab/ip_sniffer/target/debug/deps/libip_sniffer-3282f7411ead1d55.rmeta: src/main.rs
-
-src/main.rs:
--- ip_sniffer/target/debug/deps/ip_sniffer-6e5b96bdfb67c791.d
+++
@@ -1,5 +0,0 @@
-/home/bolo/Documents/GitHub/rustLab/ip_sniffer/target/debug/deps/ip_sniffer-6e5b96bdfb67c791.d: src/main.rs
-
-/home/bolo/Documents/GitHub/rustLab/ip_sniffer/target/debug/deps/libip_sniffer-6e5b96bdfb67c791.rmeta: src/main.rs
-
-src/main.rs:
